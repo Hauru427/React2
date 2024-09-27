9b6450959cd01c96b1bb8ad5e0118c8301bd93f0
Hauru427 <hauru.051427@gmail.com>
2024-09-27 23:14:27 +0900

    とりあえずコンポーネントの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import { useState } from 'react';
 import './App.css';
+import { ChildArea } from './ChildArea';
 
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
 
-  const onClickCountUp = () => {
-    setCount(count + 1);
-  };
+  const onChangeText = (e) => setText(e.target.value);
+
+  const onClickOpen = () => setOpen(!open);
 
   return (
     <div className="App">
-      <input />
+      <input value={text} onChange={onChangeText} />
       <br />
       <br />
-      <button>表示</button>
-      
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open}/>
     </div>
   );
 }
